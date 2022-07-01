dc71b9e475124df23df7e16ad50b06a5e79a5342
onojaonoja2 <samuelonoja06@gmail.com>
2022-07-01 04:10:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-/* main and return are defined */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-Return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
