523091f6b33c0e40c181302669ab9a0b08e205d0
onojaonoja2 <samuelonoja06@gmail.com>
2022-07-01 04:20:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-/* entry and output well laid */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-Return(0);
+	return (0);
 }
