647960ac7c02b2a7d61a773435e6d7cd523f0bff
onojaonoja2 <samuelonoja06@gmail.com>
2022-07-12 15:58:29 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,11 @@ void puts_half(char *str)
 			break;
 		count++;
 	}
+
 	if (count % 2 == 1)
-		i == count / 2;
+		i = count / 2;
 	else
-	       	i = (count - 1) / 2;
+		i = (count - 1) / 2;
 
 	for (i++; i < count; i++)
 		_putchar(str[i]);
