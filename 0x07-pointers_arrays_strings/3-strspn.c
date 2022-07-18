02717d54fb47b1cbb75f9f66167fc4714c271c52
onojaonoja2 <samuelonoja06@gmail.com>
2022-07-18 15:03:49 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of a prefix
+ * @s: string where substacting will look.
+ * @accept: substring of accepted chars.
+ * Return: length of occurence.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
